a9c9a6976ce31f40731a963feaa3eadeb193b2c1
julian-muenzer-snkeos <julian.muenzer@snkeos.com>
2025-01-14 20:07:00 +0100

    add loading animation

--- lib/main.dart
+++ lib/main.dart
@@ -289,7 +289,98 @@ class QueensGameApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const QueensGameScreen(),
+      home: const LoadingScreen(),
+    );
+  }
+}
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _rotationAnimation;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+
+    _rotationAnimation = Tween<double>(
+      begin: 0,
+      end: 4 * 3.14159,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOutQuart,
+    ));
+
+    _scaleAnimation = TweenSequence<double>([
+      TweenSequenceItem(
+        tween: Tween<double>(begin: 0.0, end: 1.2)
+            .chain(CurveTween(curve: Curves.easeOut)),
+        weight: 40,
+      ),
+      TweenSequenceItem(
+        tween: Tween<double>(begin: 1.2, end: 1.0)
+            .chain(CurveTween(curve: Curves.easeInOut)),
+        weight: 60,
+      ),
+    ]).animate(_controller);
+
+    _controller.forward().then((_) {
+      Navigator.of(context).pushReplacement(
+        PageRouteBuilder(
+          pageBuilder: (context, animation, secondaryAnimation) => const QueensGameScreen(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+          transitionDuration: const Duration(milliseconds: 500),
+        ),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[50],
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Transform.scale(
+              scale: _scaleAnimation.value,
+              child: Transform.rotate(
+                angle: _rotationAnimation.value,
+                child: SvgPicture.string(
+                  '''<svg width="64" height="64" viewBox="0 0 24 24" fill="black">
+                    <path d="M12 2L8 7L3 4L4 10L2 16H22L20 10L21 4L16 7L12 2ZM12 4.5L14.5 7.8L12 10L9.5 7.8L12 4.5ZM6.9 14L8 10.2L10.5 12.5L12 10L13.5 12.5L16 10.2L17.1 14H6.9Z"/>
+                  </svg>''',
+                  width: 64,
+                  height: 64,
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
